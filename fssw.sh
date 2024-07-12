12ed6f33228f014c214907954f5a5a66869f59e4
shwelu2024 <118778546+shwelu2024@users.noreply.github.com>
2024-07-12 14:28:38 +0700

    Add files via upload

--- fssw.sh
+++ fssw.sh
@@ -1,6 +1,6 @@
 wget -q -O /etc/systemd/system/ws-tls.service "https://raw.githubusercontent.com/shwelu2024/auto-script/main/websocket/ws-tls.service" && chmod +x /etc/systemd/system/ws-tls.service
 wget -q -O /usr/local/bin/ws-tls "https://raw.githubusercontent.com/shwelu2024/auto-script/main/websocket/ws-tls" && chmod +x /usr/local/bin/ws-tls
-wget -q -O /etc/stunnel5/stunnel5.conf "https://raw.githubusercontent.com/NevermoreSSH/FIX/main/stunnel5.conf" && chmod +x /etc/stunnel5/stunnel5.conf
+wget -q -O /etc/stunnel5/stunnel5.conf "https://raw.githubusercontent.com/shwelu2024/fix/main/stunnel5.conf" && chmod +x /etc/stunnel5/stunnel5.conf
 
 # Settings SSLH
 cat > /etc/default/sslh <<-END
